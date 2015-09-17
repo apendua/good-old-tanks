8db757b20f81528a578ee1516230a9da46b19fcb
Dawid Rusnak <dawid@drcode.pl>
2015-09-17 21:49:34 +0200

    Fix connection & tank shooting / moving

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 'use strict';
 
 function random(from, to) {
-    return (Math.random() * to + from) % to;
+    return Math.ceil((Math.random() * to + from) % to);
 }
 
 var settings = require('./settings.json'),
     connection = new (require('./lib/Connection'))(settings.apiUrl, settings.secret),
-    game = new (require('./lib/Game'))(connection);
+    game = new (require('./lib/Game'))(connection, settings.tournament);
 
 function shootRandomly() {
-    var angle = random(-90, 90);
+    var angle = random(0, 180) - 90;
     var power = random(1, 100);
 
     game.tank.shoot(angle, power).then(function(data) {
-        console.log(JSON.stringify(data));
         shootRandomly();
     })
 }
 
-game.start('DROP TABLE', false).then(function() {
+game.start(settings.username, false).then(function() {
     console.log('Game started, waiting for play');
 
     shootRandomly();
